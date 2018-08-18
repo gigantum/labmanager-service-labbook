7ecfccb7e18e6b90b7be82c81c8ca451f5e30b57
Bill Van Besien <bill@gigantum.io>
2018-08-18 11:58:17 -0400

    Moved all environment components into one file
    Signed-off-by: Bill Van Besien <bill@gigantum.io>

--- lmsrvlabbook/api/objects/envcomponents.py
+++ lmsrvlabbook/api/objects/envcomponents.py
@@ -19,7 +19,11 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 import graphene
+
 from lmcommon.environment import ComponentRepository
+from lmcommon.logging import LMLogger
+
+logger = LMLogger.get_logger()
 
 
 class BaseComponent(graphene.ObjectType, interfaces=(graphene.relay.Node,)):
@@ -262,3 +266,218 @@ class BaseComponent(graphene.ObjectType, interfaces=(graphene.relay.Node,)):
         if self.installed_packages is None:
             self._load_component_info()
         return self.installed_packages
+
+
+class PackageComponentInput(graphene.InputObjectType):
+    """An input type to support Batch interfaces that use Package Components"""
+
+    # The name of the package manager
+    manager = graphene.String(required=True)
+
+    # The name of the package
+    package = graphene.String(required=True)
+
+    # The desired version of the package
+    version = graphene.String()
+
+
+class PackageComponent(graphene.ObjectType, interfaces=(graphene.relay.Node,)):
+    """A type that represents a Package Manager based Environment Component"""
+    # The loaded yaml data
+    _component_data = None
+
+    # The version dataloader used to coalesce conda commands
+    _version_dataloader = None
+
+    # The Component schema version
+    schema = graphene.Int()
+
+    # The name of the package manager
+    manager = graphene.String(required=True)
+
+    # The name of the package
+    package = graphene.String(required=True)
+
+    # The current version of the package
+    version = graphene.String(required=True)
+
+    # The latest available version of the package
+    latest_version = graphene.String()
+
+    # Flag indicating if the component is in the Base
+    from_base = graphene.Boolean()
+
+    # Flag indicating if the package name and version has been validated
+    is_valid = graphene.Boolean(default_value=False)
+
+    @classmethod
+    def get_node(cls, info, id):
+        """Method to resolve the object based on it's Node ID"""
+        # Parse the key
+        manager, package, version = id.split("&")
+
+        return PackageComponent(id=f"{manager}&{package}&{version}",
+                                manager=manager, package=package, version=version)
+
+    def resolve_id(self, info):
+        """Resolve the unique Node id for this object"""
+        if not self.manager or not self.package or self.version is None:
+            raise ValueError("Resolving a PackageComponent ID requires manager, package and version to be set")
+
+        return f"{self.manager}&{self.package}&{self.version}"
+
+    def resolve_latest_version(self, info):
+        """Resolve the latest_version field"""
+        if self.latest_version is not None:
+            return self.latest_version
+
+        if not self._version_dataloader:
+            # No dataloader is available, so load manually
+            logger.warning("Cannot query for latest version of {self.package} - no labbook context")
+            return None
+
+        # If you get here, load via version dataloader (which has labbook context)
+        return self._version_dataloader.load(f"{self.manager}&{self.package}")
+
+    def resolve_from_base(self, info):
+        """Resolve the from_base field"""
+        if self.from_base is None:
+            # Assume not from base if value is not set on construction
+            return False
+        else:
+            return self.from_base
+
+
+class CustomComponent(graphene.ObjectType, interfaces=(graphene.relay.Node,)):
+    """A type that represents a Custom Dependency Environment Component"""
+    # The loaded yaml data
+    _component_data = None
+
+    # The Component schema version
+    schema = graphene.Int()
+
+    # The name of the component repository where this Base is stored
+    repository = graphene.String(required=True)
+
+    # id field from the Base component definition file
+    component_id = graphene.String(required=True)
+
+    # Revision of the component
+    revision = graphene.Int(required=True)
+
+    # Human readable name
+    name = graphene.String()
+
+    # Short description of the Base
+    description = graphene.String()
+
+    # Tags that can be used when searching/filtering components
+    tags = graphene.List(graphene.String)
+
+    # License applied to the underlying component being installed
+    license = graphene.String()
+
+    # Required OS base class to add the component
+    os_base_class = graphene.String()
+
+    # Url to more documentation or info about the base
+    url = graphene.String()
+
+    # List of installed package managers that are available to the user
+    required_package_managers = graphene.List(graphene.String)
+
+    # Custom snippet to insert into the LabBook's Dockerfile
+    docker_snippet = graphene.String()
+
+    def _load_component_info(self):
+        """Private method to retrieve file info for a given key"""
+        if not self._component_data:
+            repo = ComponentRepository()
+
+            self._component_data = repo.get_component("custom", self.repository, self.component_id, self.revision)
+
+        self.schema = self._component_data['schema']
+        self.name = self._component_data['name']
+        self.description = self._component_data['description']
+        self.tags = self._component_data.get('tags')
+        self.license = self._component_data.get('license')
+        self.url = self._component_data.get('url')
+        self.os_base_class = self._component_data.get('os_base_class')
+        self.required_package_managers = self._component_data.get('required_package_managers')
+        self.docker_snippet = self._component_data['docker']
+
+    @classmethod
+    def get_node(cls, info, id):
+        """Method to resolve the object based on it's Node ID"""
+        # Parse the key
+        repository, component_id, revision = id.split("&")
+
+        return CustomComponent(id=f"{repository}&{component_id}&{revision}",
+                               repository=repository, component_id=component_id, revision=int(revision))
+
+    def resolve_id(self, info):
+        """Resolve the unique Node id for this object"""
+        if not self.repository or not self.component_id or self.revision is None:
+            raise ValueError("Resolving a CustomComponent ID requires repository, component_id and revision to be set")
+
+        return f"{self.repository}&{self.component_id}&{self.revision}"
+
+    def resolve_schema(self, info):
+        """Resolve the schema field"""
+        if self.schema is None:
+            self._load_component_info()
+        return self.schema
+
+    def resolve_name(self, info):
+        """Resolve the name field"""
+        if self.name is None:
+            self._load_component_info()
+        return self.name
+
+    def resolve_description(self, info):
+        """Resolve the description field"""
+        if self.description is None:
+            self._load_component_info()
+        return self.description
+
+    def resolve_version(self, info):
+        """Resolve the version field"""
+        if self.version is None:
+            self._load_component_info()
+        return self.version
+
+    def resolve_tags(self, info):
+        """Resolve the tags field"""
+        if self.tags is None:
+            self._load_component_info()
+        return self.tags
+
+    def resolve_license(self, info):
+        """Resolve the license field"""
+        if self.license is None:
+            self._load_component_info()
+        return self.license
+
+    def resolve_url(self, info):
+        """Resolve the url field"""
+        if self.url is None:
+            self._load_component_info()
+        return self.url
+
+    def resolve_os_base_class(self, info):
+        """Resolve the os_base_class field"""
+        if self.os_base_class is None:
+            self._load_component_info()
+        return self.os_base_class
+
+    def resolve_required_package_managers(self, info):
+        """Resolve the required_package_managers field"""
+        if self.required_package_managers is None:
+            self._load_component_info()
+        return self.required_package_managers
+
+    def resolve_docker_snippet(self, info):
+        """Resolve the docker_snippet field"""
+        if self.docker_snippet is None:
+            self._load_component_info()
+        return self.docker_snippet
