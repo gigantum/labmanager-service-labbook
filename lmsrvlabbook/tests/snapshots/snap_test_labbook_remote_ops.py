8bc6c3c420c9ee761ce39275b31dc1a82ff27e81
Dean Kleissas <dean.kleissas@gmail.com>
2018-09-11 10:33:02 -0400

    GIG-2159: Cloud project listing improvement (#176)

--- lmsrvlabbook/tests/snapshots/snap_test_labbook_remote_ops.py
+++ lmsrvlabbook/tests/snapshots/snap_test_labbook_remote_ops.py
@@ -7,80 +7,6 @@ from snapshottest import Snapshot
 
 snapshots = Snapshot()
 
-snapshots['TestLabBookRemoteOperations.test_list_remote_labbooks_az 1'] = {
-    'data': {
-        'labbookList': {
-            'remoteLabbooks': {
-                'edges': [
-                    {
-                        'cursor': 'MA==',
-                        'node': {
-                            'creationDateUtc': '2018-04-19T19:36:11.009Z',
-                            'description': '',
-                            'id': 'UmVtb3RlTGFiYm9vazp0ZXN0dXNlciZ0ZXN0Mg==',
-                            'isLocal': False,
-                            'modifiedDateUtc': '2018-04-19T20:58:05.974Z',
-                            'name': 'test2',
-                            'owner': 'testuser'
-                        }
-                    },
-                    {
-                        'cursor': 'MA==',
-                        'node': {
-                            'creationDateUtc': '2018-04-19T19:06:11.009Z',
-                            'description': '',
-                            'id': 'UmVtb3RlTGFiYm9vazp0ZXN0dXNlciZ0ZXN0MTE=',
-                            'isLocal': False,
-                            'modifiedDateUtc': '2018-04-19T22:08:05.974Z',
-                            'name': 'test11',
-                            'owner': 'testuser'
-                        }
-                    }
-                ],
-                'pageInfo': {
-                    'hasNextPage': True
-                }
-            }
-        }
-    }
-}
-
-snapshots['TestLabBookRemoteOperations.test_list_remote_labbooks_az 2'] = {
-    'data': {
-        'labbookList': {
-            'remoteLabbooks': {
-                'edges': [
-                    {
-                        'cursor': 'MA==',
-                        'node': {
-                            'creationDateUtc': '2018-04-19T19:06:11.009Z',
-                            'description': '',
-                            'id': 'UmVtb3RlTGFiYm9vazp0ZXN0dXNlciZ0ZXN0MTE=',
-                            'modifiedDateUtc': '2018-04-19T22:08:05.974Z',
-                            'name': 'test11',
-                            'owner': 'testuser'
-                        }
-                    },
-                    {
-                        'cursor': 'MA==',
-                        'node': {
-                            'creationDateUtc': '2018-04-19T19:36:11.009Z',
-                            'description': '',
-                            'id': 'UmVtb3RlTGFiYm9vazp0ZXN0dXNlciZ0ZXN0Mg==',
-                            'modifiedDateUtc': '2018-04-19T20:58:05.974Z',
-                            'name': 'test2',
-                            'owner': 'testuser'
-                        }
-                    }
-                ],
-                'pageInfo': {
-                    'hasNextPage': False
-                }
-            }
-        }
-    }
-}
-
 snapshots['TestLabBookRemoteOperations.test_list_remote_labbooks_invalid_args 1'] = {
     'data': {
         'labbookList': {
@@ -118,13 +44,87 @@ snapshots['TestLabBookRemoteOperations.test_list_remote_labbooks_invalid_args 2'
                     'line': 4
                 }
             ],
-            'message': 'Unsupported sort_str: asdf. Use `desc`, `asc`',
+            'message': 'Unsupported sort: asdf. Use `desc`, `asc`',
             'path': [
                 'labbookList',
                 'remoteLabbooks'
             ]
         }
     ]
+}
+
+snapshots['TestLabBookRemoteOperations.test_list_remote_labbooks_az 1'] = {
+    'data': {
+        'labbookList': {
+            'remoteLabbooks': {
+                'edges': [
+                    {
+                        'cursor': 'MA==',
+                        'node': {
+                            'creationDateUtc': '2018-08-30T18:01:33.312Z',
+                            'description': 'No Description',
+                            'id': 'UmVtb3RlTGFiYm9vazp0ZXN0ZXImdGVzdC1wcm9qLTE=',
+                            'isLocal': False,
+                            'modifiedDateUtc': '2018-08-30T18:01:33.312Z',
+                            'name': 'test-proj-1',
+                            'owner': 'tester'
+                        }
+                    },
+                    {
+                        'cursor': 'MA==',
+                        'node': {
+                            'creationDateUtc': '2018-08-29T18:01:33.312Z',
+                            'description': 'No Description',
+                            'id': 'UmVtb3RlTGFiYm9vazp0ZXN0ZXImdGVzdC1wcm9qLTI=',
+                            'isLocal': False,
+                            'modifiedDateUtc': '2018-09-01T18:01:33.312Z',
+                            'name': 'test-proj-2',
+                            'owner': 'tester'
+                        }
+                    }
+                ],
+                'pageInfo': {
+                    'hasNextPage': True
+                }
+            }
+        }
+    }
+}
+
+snapshots['TestLabBookRemoteOperations.test_list_remote_labbooks_az 2'] = {
+    'data': {
+        'labbookList': {
+            'remoteLabbooks': {
+                'edges': [
+                    {
+                        'cursor': 'MA==',
+                        'node': {
+                            'creationDateUtc': '2018-08-29T18:01:33.312Z',
+                            'description': 'No Description',
+                            'id': 'UmVtb3RlTGFiYm9vazp0ZXN0ZXImdGVzdC1wcm9qLTI=',
+                            'modifiedDateUtc': '2018-09-01T18:01:33.312Z',
+                            'name': 'test-proj-2',
+                            'owner': 'tester'
+                        }
+                    },
+                    {
+                        'cursor': 'MA==',
+                        'node': {
+                            'creationDateUtc': '2018-08-30T18:01:33.312Z',
+                            'description': 'No Description',
+                            'id': 'UmVtb3RlTGFiYm9vazp0ZXN0ZXImdGVzdC1wcm9qLTE=',
+                            'modifiedDateUtc': '2018-08-30T18:01:33.312Z',
+                            'name': 'test-proj-1',
+                            'owner': 'tester'
+                        }
+                    }
+                ],
+                'pageInfo': {
+                    'hasNextPage': False
+                }
+            }
+        }
+    }
 }
 
 snapshots['TestLabBookRemoteOperations.test_list_remote_labbooks_modified 1'] = {
@@ -135,25 +135,25 @@ snapshots['TestLabBookRemoteOperations.test_list_remote_labbooks_modified 1'] = 
                     {
                         'cursor': 'MA==',
                         'node': {
-                            'creationDateUtc': '2018-04-19T19:06:11.009Z',
-                            'description': '',
-                            'id': 'UmVtb3RlTGFiYm9vazp0ZXN0dXNlciZ0ZXN0MTE=',
+                            'creationDateUtc': '2018-08-29T18:01:33.312Z',
+                            'description': 'No Description',
+                            'id': 'UmVtb3RlTGFiYm9vazp0ZXN0ZXImdGVzdC1wcm9qLTI=',
                             'isLocal': False,
-                            'modifiedDateUtc': '2018-04-19T22:08:05.974Z',
-                            'name': 'test11',
-                            'owner': 'testuser'
+                            'modifiedDateUtc': '2018-09-01T18:01:33.312Z',
+                            'name': 'test-proj-2',
+                            'owner': 'tester'
                         }
                     },
                     {
                         'cursor': 'MA==',
                         'node': {
-                            'creationDateUtc': '2018-04-19T19:36:11.009Z',
-                            'description': '',
-                            'id': 'UmVtb3RlTGFiYm9vazp0ZXN0dXNlciZ0ZXN0Mg==',
+                            'creationDateUtc': '2018-08-30T18:01:33.312Z',
+                            'description': 'No Description',
+                            'id': 'UmVtb3RlTGFiYm9vazp0ZXN0ZXImdGVzdC1wcm9qLTE=',
                             'isLocal': False,
-                            'modifiedDateUtc': '2018-04-19T20:58:05.974Z',
-                            'name': 'test2',
-                            'owner': 'testuser'
+                            'modifiedDateUtc': '2018-08-30T18:01:33.312Z',
+                            'name': 'test-proj-1',
+                            'owner': 'tester'
                         }
                     }
                 ],
@@ -173,23 +173,23 @@ snapshots['TestLabBookRemoteOperations.test_list_remote_labbooks_modified 2'] = 
                     {
                         'cursor': 'MA==',
                         'node': {
-                            'creationDateUtc': '2018-04-19T19:36:11.009Z',
-                            'description': '',
-                            'id': 'UmVtb3RlTGFiYm9vazp0ZXN0dXNlciZ0ZXN0Mg==',
-                            'modifiedDateUtc': '2018-04-19T20:58:05.974Z',
-                            'name': 'test2',
-                            'owner': 'testuser'
+                            'creationDateUtc': '2018-08-30T18:01:33.312Z',
+                            'description': 'No Description',
+                            'id': 'UmVtb3RlTGFiYm9vazp0ZXN0ZXImdGVzdC1wcm9qLTE=',
+                            'modifiedDateUtc': '2018-08-30T18:01:33.312Z',
+                            'name': 'test-proj-1',
+                            'owner': 'tester'
                         }
                     },
                     {
                         'cursor': 'MA==',
                         'node': {
-                            'creationDateUtc': '2018-04-19T19:06:11.009Z',
-                            'description': '',
-                            'id': 'UmVtb3RlTGFiYm9vazp0ZXN0dXNlciZ0ZXN0MTE=',
-                            'modifiedDateUtc': '2018-04-19T22:08:05.974Z',
-                            'name': 'test11',
-                            'owner': 'testuser'
+                            'creationDateUtc': '2018-08-29T18:01:33.312Z',
+                            'description': 'No Description',
+                            'id': 'UmVtb3RlTGFiYm9vazp0ZXN0ZXImdGVzdC1wcm9qLTI=',
+                            'modifiedDateUtc': '2018-09-01T18:01:33.312Z',
+                            'name': 'test-proj-2',
+                            'owner': 'tester'
                         }
                     }
                 ],
@@ -209,25 +209,25 @@ snapshots['TestLabBookRemoteOperations.test_list_remote_labbooks_created 1'] = {
                     {
                         'cursor': 'MA==',
                         'node': {
-                            'creationDateUtc': '2018-04-19T19:36:11.009Z',
-                            'description': '',
-                            'id': 'UmVtb3RlTGFiYm9vazp0ZXN0dXNlciZ0ZXN0Mg==',
+                            'creationDateUtc': '2018-08-30T18:01:33.312Z',
+                            'description': 'No Description',
+                            'id': 'UmVtb3RlTGFiYm9vazp0ZXN0ZXImdGVzdC1wcm9qLTE=',
                             'isLocal': False,
-                            'modifiedDateUtc': '2018-04-19T20:58:05.974Z',
-                            'name': 'test2',
-                            'owner': 'testuser'
+                            'modifiedDateUtc': '2018-08-30T18:01:33.312Z',
+                            'name': 'test-proj-1',
+                            'owner': 'tester'
                         }
                     },
                     {
                         'cursor': 'MA==',
                         'node': {
-                            'creationDateUtc': '2018-04-19T19:06:11.009Z',
-                            'description': '',
-                            'id': 'UmVtb3RlTGFiYm9vazp0ZXN0dXNlciZ0ZXN0MTE=',
+                            'creationDateUtc': '2018-08-29T18:01:33.312Z',
+                            'description': 'No Description',
+                            'id': 'UmVtb3RlTGFiYm9vazp0ZXN0ZXImdGVzdC1wcm9qLTI=',
                             'isLocal': False,
-                            'modifiedDateUtc': '2018-04-19T22:08:05.974Z',
-                            'name': 'test11',
-                            'owner': 'testuser'
+                            'modifiedDateUtc': '2018-09-01T18:01:33.312Z',
+                            'name': 'test-proj-2',
+                            'owner': 'tester'
                         }
                     }
                 ],
@@ -247,23 +247,23 @@ snapshots['TestLabBookRemoteOperations.test_list_remote_labbooks_created 2'] = {
                     {
                         'cursor': 'MA==',
                         'node': {
-                            'creationDateUtc': '2018-04-19T19:06:11.009Z',
-                            'description': '',
-                            'id': 'UmVtb3RlTGFiYm9vazp0ZXN0dXNlciZ0ZXN0MTE=',
-                            'modifiedDateUtc': '2018-04-19T22:08:05.974Z',
-                            'name': 'test11',
-                            'owner': 'testuser'
+                            'creationDateUtc': '2018-08-29T18:01:33.312Z',
+                            'description': 'No Description',
+                            'id': 'UmVtb3RlTGFiYm9vazp0ZXN0ZXImdGVzdC1wcm9qLTI=',
+                            'modifiedDateUtc': '2018-09-01T18:01:33.312Z',
+                            'name': 'test-proj-2',
+                            'owner': 'tester'
                         }
                     },
                     {
                         'cursor': 'MA==',
                         'node': {
-                            'creationDateUtc': '2018-04-19T19:36:11.009Z',
-                            'description': '',
-                            'id': 'UmVtb3RlTGFiYm9vazp0ZXN0dXNlciZ0ZXN0Mg==',
-                            'modifiedDateUtc': '2018-04-19T20:58:05.974Z',
-                            'name': 'test2',
-                            'owner': 'testuser'
+                            'creationDateUtc': '2018-08-30T18:01:33.312Z',
+                            'description': 'No Description',
+                            'id': 'UmVtb3RlTGFiYm9vazp0ZXN0ZXImdGVzdC1wcm9qLTE=',
+                            'modifiedDateUtc': '2018-08-30T18:01:33.312Z',
+                            'name': 'test-proj-1',
+                            'owner': 'tester'
                         }
                     }
                 ],
@@ -283,13 +283,13 @@ snapshots['TestLabBookRemoteOperations.test_list_remote_labbooks_page 1'] = {
                     {
                         'cursor': 'MA==',
                         'node': {
-                            'creationDateUtc': '2018-04-19T19:36:11.009Z',
-                            'description': '',
-                            'id': 'UmVtb3RlTGFiYm9vazp0ZXN0dXNlciZ0ZXN0Mg==',
+                            'creationDateUtc': '2018-08-30T18:01:33.312Z',
+                            'description': 'No Description',
+                            'id': 'UmVtb3RlTGFiYm9vazp0ZXN0ZXImdGVzdC1wcm9qLTE=',
                             'isLocal': False,
-                            'modifiedDateUtc': '2018-04-19T20:58:05.974Z',
-                            'name': 'test2',
-                            'owner': 'testuser'
+                            'modifiedDateUtc': '2018-08-30T18:01:33.312Z',
+                            'name': 'test-proj-1',
+                            'owner': 'tester'
                         }
                     }
                 ],
@@ -309,12 +309,12 @@ snapshots['TestLabBookRemoteOperations.test_list_remote_labbooks_page 2'] = {
                     {
                         'cursor': 'MQ==',
                         'node': {
-                            'creationDateUtc': '2018-04-19T19:06:11.009Z',
-                            'description': '',
-                            'id': 'UmVtb3RlTGFiYm9vazp0ZXN0dXNlciZ0ZXN0MTE=',
-                            'modifiedDateUtc': '2018-04-19T22:08:05.974Z',
-                            'name': 'test11',
-                            'owner': 'testuser'
+                            'creationDateUtc': '2018-08-29T18:01:33.312Z',
+                            'description': 'No Description',
+                            'id': 'UmVtb3RlTGFiYm9vazp0ZXN0ZXImdGVzdC1wcm9qLTI=',
+                            'modifiedDateUtc': '2018-09-01T18:01:33.312Z',
+                            'name': 'test-proj-2',
+                            'owner': 'tester'
                         }
                     }
                 ],
